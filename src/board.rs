a9c5711a9b909778a255a511d5e1a3f53c589a45
Chris C Cerami <chrisccerami@gmail.com>
2016-02-12 23:20:43 -0500

    I'm dumb

--- src/board.rs
+++ src/board.rs
@@ -33,7 +33,15 @@ impl Board {
     }
 
     pub fn get_winner(&self) -> Option<Piece> {
-        self.has_vertical_win()
+        let mut winner = None;
+        let vertical_win = self.has_vertical_win();
+        let horizontal_win = self.has_horizontal_win();
+        if horizontal_win.is_some() {
+            winner = horizontal_win;
+        } else if vertical_win.is_some() {
+            winner = vertical_win;
+        }
+        winner
     }
 
     fn has_vertical_win(&self) -> Option<Piece> {
@@ -42,6 +50,26 @@ impl Board {
             for column in &self.columns {
                 for index in (HEIGHT-3)..HEIGHT {
                     if column[(index-3)..].iter().all(|&piece| piece.is_some() && piece.unwrap() == *player) {
+                        winner = Some(*player)
+                    }
+                }
+            }
+        }
+        winner
+    }
+
+    fn has_horizontal_win(&self) -> Option<Piece> {
+        let mut winner = None;
+        for player in &[Piece::Red, Piece::Black] {
+            for starting_index in 0..(WIDTH-3) {
+                for row in 0..(HEIGHT) {
+                    let mut pieces = [None; 4];
+                    let mut counter = 0;
+                    for index in starting_index..(starting_index+3) {
+                        pieces[counter] = self.columns[index][row];
+                        counter += 1;
+                    }
+                    if pieces.iter().all(|&piece| piece.is_some() && piece.unwrap() == *player ) {
                         winner = Some(*player)
                     }
                 }
